804fc000f2b9bd36f1740fdbaf4baba3523099b6
Sean Wong <serotonincrash@gmail.com>
2026-08-01 13:44:19 +0800

    build!: raise minimum deployment targets to iOS 17/macOS 14/watchOS 10/tvOS 17
    BREAKING CHANGE: clients now require iOS 17, macOS 14, watchOS 10, tvOS 17 and Xcode 15+.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "Forever",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v11),
-        .watchOS(.v6),
-        .tvOS(.v13)
+        .iOS(.v17),
+        .macOS(.v14),
+        .watchOS(.v10),
+        .tvOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
